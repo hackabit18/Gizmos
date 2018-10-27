03133592c81461669b2c07f7336ce52b1d86c284
Samuelimza <osamaazmi1999@gmail.com>
2018-10-27 16:24:13 +0530

    file inputOutput

--- textManipulation/main.cpp
+++ textManipulation/main.cpp
@@ -2,11 +2,25 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+
 using namespace std;
 
 int main(){
+	vector<string> keyWords;
+	ifstream file;
+	file.open("keywords.txt");
+	int counter = 0;
+	while(!file.eof()){
+		string testLine;
+		getline(file, testLine);
+		keyWords.push_back(testLine);
+	}
+	cout<<keyWords.size();
+	for (int i = 0; i < keyWords.size(); i++){
+		cout<<keyWords[i]<<endl;
+	}
 	string line("Osama Ashhad Azmi means to a p r e h e n d y o u a l l now that he is upset");
 	cout<<removeWhiteSpaces(line, 27, 55);
-	
 	cin.get();
 }
